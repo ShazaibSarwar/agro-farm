f7dfe16874fa1993225273a97ee68170628cc756
Shazaib Sarwar <shazaibs355@gmail.com>
2023-02-12 17:18:45 +0500

    Socket Issue Fixed

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
-  process.exit(1);
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
 });
 
 dotenv.config({path: './config.env'});
@@ -36,24 +36,33 @@ process.on('unhandledRejection', err => {
 let io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:3000',
+        origin: '*',
+        // origin: 'http://localhost:3000',
         // credentials: true,
     }
 });
 
 io.on('connection', (socket) => {
+
     console.log("Connected to socket.io");
-    require('./routes/messageRoutes')(io, socket);
     socket.on("setup", (userData) => {
         socket.join(userData._id);
         socket.emit("connected");
     });
+
     socket.on("join chat", (room) => {
         socket.join(room);
         console.log("User Joined Room: " + room);
     });
+
+    socket.on("leave chat", (room) => {
+        socket.leave(room);
+        console.log("User Left Room: " + room);
+    });
+
     socket.on("typing", (room) => socket.in(room).emit("typing"));
     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
     socket.on("new message", (newMessageReceived) => {
         let chat = newMessageReceived.chat;
 
@@ -61,8 +70,8 @@ io.on('connection', (socket) => {
 
         chat.users.forEach((user) => {
             if (user._id === newMessageReceived.sender._id) return;
-
             socket.in(user._id).emit("message received", newMessageReceived);
+            console.log("Message sent to: " + user.firstName + " with email  " + user.email);
         });
     });
     socket.off("setup", () => {
